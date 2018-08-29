1286cf8d77b86ccf625237cf70ebc6d0b5d92228
Rico <henddr2@usenhendrr.local>
2018-08-29 09:02:19 -0400

    Can now package a jar without gradle or maven.

--- clj/init-cider.clj
+++ clj/init-cider.clj
@@ -1,6 +1,7 @@
 (ns user
-  (:require [clojure.tools.nrepl.server :refer (start-server stop-server)]
-            [cider.nrepl :refer (cider-nrepl-handler)]))
+  (:require [cider.nrepl :refer (cider-nrepl-handler)]
+            #_[clojure.tools.nrepl.server :refer (start-server stop-server)]
+            [nrepl.server :refer (start-server stop-server)]))
 
 ; Start a cider nrepl for vim-fireplace. Requires java 1.8 for 'gf' to work in vim-fireplace.
 (defonce server (start-server :port (Integer. (or (System/getenv "NREPL_PORT") 7888))
