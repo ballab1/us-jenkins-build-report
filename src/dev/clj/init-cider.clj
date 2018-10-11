63a1fbb8fdf1e713e8024da2a5d8f7f5979b1b9f
Rico <henddr2@usenhendrr.local>
2018-10-11 00:06:10 -0400

    Preparing for src/dev subtree.

--- src/dev/clj/init-cider.clj
+++
@@ -1,9 +0,0 @@
-(ns user
-  (:require [cider.nrepl :refer (cider-nrepl-handler)]
-            #_[clojure.tools.nrepl.server :refer (start-server stop-server)]
-            [nrepl.server :refer (start-server stop-server)]))
-
-; Start a cider nrepl for vim-fireplace. Requires java 1.8 for 'gf' to work in vim-fireplace.
-(defonce server (start-server :port (Integer. (or (System/getenv "NREPL_PORT") 7888))
-                              :bind "localhost"                  ; avoid ipv4 vs ipv6 issues
-                              :handler cider-nrepl-handler))
